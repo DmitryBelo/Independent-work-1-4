5fa82f346a08d4618aca4cb6ac47f9a35c1cb8a3
DmitryBelo <Bdsxx@yandex.ru>
2023-07-05 14:43:09 +0300

    Create Cамостоятельная работа1.4.py

---
+++ Cамостоятельная работа1.4.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 13:42:16 2023
+
+@author: Student
+"""
+#Задание 1
+'''a = int(input())
+for i in range(1, a+1):
+    print(i ** 2)'''
+    
+    
+# Задание 2
+'''n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+n2 = []
+for i in range(len(n)):
+     if n[i] % 2 == 0:
+         n2.append(n[i])
+print(n2)'''
+
+
+
+# Задачя 3
+'''n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+c = 0
+for i in range(len(n)):
+    c = c + n[i]
+print(c)'''
+
+
+# Задание 4
+'''m = 'Hello'
+b = []
+for i in range(15):
+    b.append(m)
+print(b)'''
+
+
+# Задание 5
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+c = 0
+for i in range(len(n)):
+    if n[i] % 2 != 0:
+        c = c + n[i]
+print(c)
